7a114f7a9717dcf47a7ffa3d010619de158440e4
André Fincato <work@andrefincato.info>
2023-09-28 17:21:41 +0200

    add devenv basic setup

---
+++ devenv.nix
@@ -0,0 +1,21 @@
+{ pkgs, ... }:
+
+{
+
+  scripts.run-server.exec = ''
+    php -S localhost:8000
+  '';
+
+  languages.php.enable = true;
+  languages.php.version = "8.2";
+  languages.php.extensions = [
+    "bz2"
+    "zlib"
+    "mbstring"
+    "openssl"
+    "gd"
+    "intl"
+  ];
+
+}
+
